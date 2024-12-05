bcc89acc8a9a10688b5fe33221af683a9068ba0c
Santiago Morris Ballesteros <152986912+santiagomorrisb@users.noreply.github.com>
2024-12-05 16:57:48 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Añadir animaciones de desvanecimiento
-    const fadeInElements = document.querySelectorAll(".fade-in");
-    fadeInElements.forEach(element => {
-        element.classList.add("show");
-    });
-});
