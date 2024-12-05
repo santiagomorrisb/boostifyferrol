3130684915d8c99c19271a41668e38c1daf6c04f
Santiago Morris Ballesteros <152986912+santiagomorrisb@users.noreply.github.com>
2024-12-05 16:37:27 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,28 +1,28 @@
-// Inicializar EmailJS con tu User ID
-emailjs.init('oweLFr6V8uZTlcO7V');
+// Footer dinámico
+const footer = document.getElementById('dynamic-footer');
 
-// Manejo del formulario de contacto
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault(); // Evitar que la página se recargue
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.innerHeight + window.scrollY;
+    const documentHeight = document.documentElement.offsetHeight;
 
-    const formMessage = document.getElementById('form-message');
-    formMessage.style.display = 'block';
-    formMessage.textContent = 'Enviando...';
-    formMessage.style.color = 'blue';
-
-    emailjs.sendForm('service_z34e3pa', 'template_r1413uw', this)
-        .then(
-            function () {
-                // Si el mensaje se envió correctamente
-                formMessage.textContent = 'Mensaje enviado correctamente. ¡Gracias por contactarnos!';
-                formMessage.style.color = 'green';
-                document.getElementById('contactForm').reset(); // Limpiar el formulario
-            },
-            function (error) {
-                // Si hubo un error
-                formMessage.textContent = 'Error al enviar el mensaje. Por favor, intenta nuevamente.';
-                formMessage.style.color = 'red';
-                console.error('Error al enviar:', error);
-            }
-        );
+    if (scrollHeight >= documentHeight) {
+        footer.classList.remove('hidden-footer');
+        footer.classList.add('visible-footer');
+    } else {
+        footer.classList.remove('visible-footer');
+        footer.classList.add('hidden-footer');
+    }
 });
+
+// Animaciones de desvanecimiento
+const fadeInElements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+fadeInElements.forEach((el) => observer.observe(el));
